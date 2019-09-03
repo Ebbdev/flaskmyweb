b06a5b21ddc5ff74fe41eec4f93be4a14056887e
Emirhan Bilge <emirhanbilgeblt@gmail.com>
2019-09-03 19:24:33 +0300

    update commit

--- backend.py
+++ backend.py
@@ -16,9 +16,26 @@ def projects():
 def about():
     return render_template("about.html")
 
+@app.route("/english/about")
+def abouten():
+    return render_template("abouten.html")
+
+@app.route("/english/projects")
+def projectsen():
+    return render_template("projectsen.html")
+
+@app.route("/english/support")
+def supporsen():
+    return render_template("support.html")
+
+
 @app.route("/supports")
 def support():
     return render_template("support.html")
+
+@app.route("/english")
+def english():
+    return render_template("english.html")
 
 
 if __name__ == "__main__":
